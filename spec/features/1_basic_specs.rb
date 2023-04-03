917741d39ff28fd23264814475280a6136ca02a1
Ben Purinton <ben.purinton@gmail.com>
2023-04-03 09:44:58 -0800

    Update 1_basic_specs.rb

--- spec/features/1_basic_specs.rb
+++ spec/features/1_basic_specs.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe "The /movies page" do
-  it "can be visited", points: 0 do
+  it "can be visited", points: 2 do
     visit "/movies"
     
     expect(page.status_code).to be(200)
